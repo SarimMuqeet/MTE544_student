a01f4a1f20ac09b35c8e4a0537a3ff83e0f55f9a
SarimMuqeet <sarim.muqeet2012@gmail.com>
2024-11-22 17:19:46 -0500

    data visualization

--- plot_errors.py
+++ plot_errors.py
@@ -13,7 +13,7 @@ def plot_errors(filename):
     first_stamp=values[0][-1]
     
     for val in values:
-        time_list.append(val[-1] - first_stamp)
+        time_list.append((val[-1] - first_stamp) / 1e9) #to get time in seconds
 
     
     
@@ -21,23 +21,23 @@ def plot_errors(filename):
 
 
     axes[0].plot([lin[len(headers) - 3] for lin in values], [lin[len(headers) - 2] for lin in values])
-    axes[0].set_title("state space")
+    axes[0].set_title("State Space for Q = 0.5, R = 0.2")
+    axes[0].set_xlabel("X (m)")  # Set x-axis label
+    axes[0].set_ylabel("Y (m)")  # Set y-axis label
     axes[0].grid()
 
     
-    axes[1].set_title("each individual state")
+    axes[1].set_title("Each individual state for Q = 0.5, R = 0.2")
     for i in range(0, len(headers) - 1):
         axes[1].plot(time_list, [lin[i] for lin in values], label= headers[i])
 
     axes[1].legend()
+    axes[1].set_xlabel("Time (s)")  # Set x-axis label
+    axes[1].set_ylabel("Recorded Data")  # Set y-axis label
     axes[1].grid()
 
     plt.show()
     
-    
-
-
-
 
 
 import argparse
